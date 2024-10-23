92b359460b7794a91ac61b42ec362797b020dc07
jvbyrnes <62322844+jvbyrnes@users.noreply.github.com>
2024-10-23 11:21:10 +1100

    transcription creation

---
+++ transcribe_audio_file.py
@@ -0,0 +1,25 @@
+import assemblyai as aai
+import os
+
+aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+
+# You can use a local filepath:
+audio_file = "./interview.mp3"
+
+# Or use a publicly-accessible URL:
+# audio_file = (
+#     "https://assembly.ai/wildfires.mp3"
+# )
+
+config = aai.TranscriptionConfig(
+  speaker_labels=True, # Identify different speakers
+  speakers_expected=2 # This is ignored for audio files with a duration less than 2 minutes.
+)
+
+transcript = aai.Transcriber().transcribe(audio_file, config)
+
+with open ("transcript.txt", "w") as f:
+  f.write(transcript.text)
+
+for utterance in transcript.utterances:
+  print(f"Speaker {utterance.speaker}: {utterance.text}")
